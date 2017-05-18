5ea815dd5b2eea3502ca345027b5c051d48adbe0
Rem Stone <remstone7@gmail.com>
2017-05-18 16:55:11 -0400

    read and write journal

--- program.py
+++ program.py
@@ -1,3 +1,4 @@
+import journal
 
 def main():
     print_header()
@@ -12,27 +13,33 @@ def print_header():
 def run_event_loop():
 
     print('What do you want to do with your journal?')
+    cmd = 'EMPTY'
+    journal_name = 'default'
+    journal_data = journal.load(journal_name)
 
-    cmd = None
-
-    while cmd != 'x':
-
+    while cmd != 'x' and cmd:
         cmd = input('[L]ist entries, [A]dd an entry, E[x]it: ')
         cmd = cmd.lower().strip()
 
         if cmd == 'l':
-            list_entries()
+            list_entries(journal_data)
         elif cmd == 'a':
-            add_entry()
-        elif cmd != 'x':
+            add_entry(journal_data)
+        elif cmd != 'x' and cmd:
             print('Sorry we don\'t understand {}'.format(cmd))
 
     print('done')
+    journal.save(journal_name, journal_data)
 
-def list_entries():
-    print('listing')
+def list_entries(data):
+    print("Your journal entries: ")
+    entries = reversed(data)
+    for idx, entry in enumerate(entries):
+        print('* [{}] {}'.format(idx+1, entry))
 
-def add_entry():
-    print('adding')
+def add_entry(data):
+    text = input("Type your entry, <enter> to exit: ")
+    journal.add_entry(text, data)
+
 
 main()
